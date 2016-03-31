218b14af76a429d8a3e61166b2cdadfe9ea3c93b
joelsaunders <jsaunders155@hotmail.com>
2016-03-31 18:27:54 +0100

    added some input error checking

--- scraperprogram.py
+++ scraperprogram.py
@@ -1,20 +1,31 @@
+# Selenium program v2
+# Added .isalpha() check to input
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import random
 
-
 # Initialise dict to store rates in
 rates_d = {}
 
-
 # Function to create list of countries to search for.
-def input_list_create(in_str):
-    return in_str.split(", ")
-
+# Only takes letters as 
+def create_input_list(prompt):
+    while True:
+        string = input(prompt).split(", ")
+        for x in string:
+            if not x.isalpha():
+                print("Please enter only enter letters")
+                break
+            continue
+        else:
+            break
+    return string
 
 def start_browser():
-    chromedriver_path ='C:\\Users\knowhow\Desktop\Python Learning\chromedriver'
+    ##chromedriver_path ='C:\\Users\knowhow\Desktop\Python Learning\chromedriver'
+    chromedriver_path ='C:\Documents and Settings\Joel\My Documents\Downloads\chromedriver'
     browser = webdriver.Chrome(executable_path = chromedriver_path)
     return browser
 
@@ -22,11 +33,10 @@ def start_browser():
 def open_page(url):
     browser.get(url)
     
-
 # Main function that finds and prints data
 def main_loop(in_list):
     for x in range(len(in_list)):
-    
+        
         # Find country entry box & clear it
         country = browser.find_element_by_id('countryName')
         country.clear()
@@ -36,7 +46,6 @@ def main_loop(in_list):
         country.send_keys(Keys.RETURN)
 
         # Avoid being flagged and make sure link clickable
-    
         seconds = 5 + (random.random() * 5)
         time.sleep(seconds)
 
@@ -48,9 +57,7 @@ def main_loop(in_list):
         rate = browser.find_element_by_xpath\
                ('//*[@id="standardRatesTable"]/tbody/tr[1]/td[2]')
         cost = rate.text
-    
         rates_d[in_list[x]] = str(cost)
-    
     
         # Set cost to Error for problems in next iteration.
         cost = "Error"
@@ -59,8 +66,8 @@ def main_loop(in_list):
 # Call functions
 if __name__ == "__main__":
 
-    in_list = input_list_create(input("Enter string of countries separated \
-by a comma then space: "))
+    in_list = create_input_list("Enter string of countries separated \
+by a comma then space: ")
     browser = start_browser()
     open_page('http://international.o2.co.uk/internationaltariffs/\
 calling_abroad_from_uk')
